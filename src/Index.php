0ae7c03faf11d1ff0ff3206c26989e5db2769e33
clark21 <clark@dev-engine.net>
2015-10-06 18:43:14 +0800

    test suites

--- src/Index.php
+++ src/Index.php
@@ -13,14 +13,14 @@ use Eden\File\Index as File;
 
 /**
  * This is an abstract definition of common
- * folder manipulation listing and information 
+ * folder manipulation listing and information
  * per folder.
  *
  * @vendor Eden
  * @package System
  * @author Christian Blanquera cblanquera@openovate.com
  */
-class Index extends Base 
+class Index extends Base
 {
     const ERROR_CHMOD_IS_INVALID = 'Invalid permissions set when creating the folder %s.';
 
@@ -30,13 +30,13 @@ class Index extends Base
      * @param int chmod
      * @return this
      */
-    public function create($chmod = 0755) 
+    public function create($chmod = 0755)
     {
         //argument 1 must be an integer
         Argument::i()->test(1, 'int');
-		
+        
         //if chmod is not and integer or not between 0 and 777
-        if(!is_int($chmod) || $chmod < 0 || $chmod > 777) {
+        if (!is_int($chmod) || $chmod < 0 || $chmod > 777) {
             //throw an error
             Exception::i(self::ERROR_CHMOD_IS_INVALID)
                 ->addVariable($this->data)
@@ -44,7 +44,7 @@ class Index extends Base
         }
 
         //if it's not a directory
-        if(!is_dir($this->data)) {
+        if (!is_dir($this->data)) {
             //then make it
             mkdir($this->data, $chmod, true);
         }
@@ -56,17 +56,17 @@ class Index extends Base
      * Returns a list of files given the path and optionally the pattern
      *
      * @param string|null regular expression
-	 * @param bool
+     * @param bool
      * @return array
      */
-    public function getFiles($regex = null, $recursive = false) 
+    public function getFiles($regex = null, $recursive = false)
     {
         //argument test
         Argument::i()
-			//argument 1 must be a string
-            ->test(1, 'string', 'null') 
-			//argument 2 must be a boolean
-            ->test(2, 'bool');          
+            //argument 1 must be a string
+            ->test(1, 'string', 'null')
+            //argument 2 must be a boolean
+            ->test(2, 'bool');
 
         $this->absolute();
 
@@ -76,12 +76,12 @@ class Index extends Base
             //for each file
             while (false !== ($file = readdir($handle))) {
                 // If this is infact a file
-                if(filetype($this->data . '/' . $file) == 'file'
-                    && (!$regex || preg_match($regex, $file))) {    
+                if (filetype($this->data . '/' . $file) == 'file'
+                    && (!$regex || preg_match($regex, $file))) {
                     //add it
-                    $files[] = File::i($this->data . '/' . $file); 
+                    $files[] = File::i($this->data . '/' . $file);
                 // recursive and this is infact a directory
-                } else if($recursive && $file != '.' && $file != '..' 
+                } else if ($recursive && $file != '.' && $file != '..'
                     && filetype($this->data .'/'. $file) == 'dir') {
                     $subfiles = self::i($this->data.'/'.$file);
                     $files = array_merge($files, $subfiles->getFiles($regex, $recursive));
@@ -98,34 +98,33 @@ class Index extends Base
      * Returns a list of folders given the path and optionally the regular expression
      *
      * @param string regular expression
-	 * @param bool
+     * @param bool
      * @return array
      */
-    public function getFolders($regex = null, $recursive = false) 
+    public function getFolders($regex = null, $recursive = false)
     {
         //argument test
         Argument::i()
-			//argument 1 must be a string
-            ->test(1, 'string', 'null') 
-			//argument 2 must be a boolean
-            ->test(2, 'bool');          
+            //argument 1 must be a string
+            ->test(1, 'string', 'null')
+            //argument 2 must be a boolean
+            ->test(2, 'bool');
 
         $this->absolute();
 
         $folders = array();
 
-        if($handle = opendir($this->data)) {
+        if ($handle = opendir($this->data)) {
             //walk the directory
             while (false !== ($folder = readdir($handle))) {
                 // If this is infact a directory
                 //and if it matches the regex
-                if($folder != '.' && $folder != '..' 
+                if ($folder != '.' && $folder != '..'
                       && filetype($this->data .'/'. $folder) == 'dir'
                     && (!$regex || preg_match($regex, $folder))) {
-
                     //add it
-                    $folders[] = self::i($this->data . '/' . $folder); 
-                    if($recursive) {
+                    $folders[] = self::i($this->data . '/' . $folder);
+                    if ($recursive) {
                         $subfolders = self::i($this->data.'/'.$folder);
                         $folders = array_merge($folders, $subfolders->getFolders($regex, $recursive));
                     }
@@ -143,26 +142,26 @@ class Index extends Base
      *
      * @return string the name
      */
-    public function getName() 
+    public function getName()
     {
         $pathArray = $this->getArray();
         return array_pop($pathArray);
     }
 
     /**
-     * Checks to see if this 
+     * Checks to see if this
      * path is a real file
      *
-	 * @param string|null
+     * @param string|null
      * @return bool
      */
-    public function isFolder($path = null) 
+    public function isFolder($path = null)
     {
-		//argument 1 must be a string
-        Argument::i()->test(1, 'string', 'null');    
+        //argument 1 must be a string
+        Argument::i()->test(1, 'string', 'null');
 
         //if path is string
-        if(is_string($path)) {
+        if (is_string($path)) {
             //return path appended
             return is_dir($this->data.'/'.$path);
         }
@@ -175,13 +174,13 @@ class Index extends Base
      *
      * @return this
      */
-    public function remove() 
+    public function remove()
     {
         //get absolute path
         $path = $this->absolute();
 
         //if it's a directory
-        if(is_dir($path)) {
+        if (is_dir($path)) {
             //remove it
             rmdir($path);
         }
@@ -195,20 +194,20 @@ class Index extends Base
      * @param string|null regular expression
      * @return this
      */
-    public function removeFiles($regex = null) 
+    public function removeFiles($regex = null)
     {
-		//argument 1 must be a string
+        //argument 1 must be a string
         Argument::i()->test(1, 'string', 'null');
 
         //get the files
         $files = $this->getFiles($regex);
 
-        if(empty($files)) {
+        if (empty($files)) {
             return $this;
         }
 
         //walk the array
-        foreach($files as $file) {
+        foreach ($files as $file) {
             //remove everything
             $file->remove();
         }
@@ -222,21 +221,21 @@ class Index extends Base
      * @param string regular expression
      * @return this
      */
-    public function removeFolders($regex = null) 
+    public function removeFolders($regex = null)
     {
-		//argument 1 must be a string or null
+        //argument 1 must be a string or null
         Argument::i()->test(1, 'string', 'null');
 
         $this->absolute();
 
         $folders = $this->getFolders($regex);
 
-        if(empty($folders)) {
+        if (empty($folders)) {
             return $this;
         }
 
         //walk directory
-        foreach($folders as $folder) {
+        foreach ($folders as $folder) {
             //remove directory
             $folder->remove();
         }
@@ -249,7 +248,7 @@ class Index extends Base
      *
      * @return this
      */
-    public function truncate() 
+    public function truncate()
     {
         $this->removeFolders();
         $this->removeFiles();
